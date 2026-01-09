ec73ee25722d611c515e4c476f0372b3312d2310
DarioConiglione <dariocng13@gmail.com>
2026-01-09 19:17:16 +0100

    add first controller and router

---
+++ controllers/productsController.js
@@ -0,0 +1,10 @@
+const connection = require('../data/db')
+
+function index(req, res) {
+
+    // da scrivere altro codice qui!
+
+}
+
+
+module.exports = { index }
--- server.js
+++ server.js
@@ -1,2 +1,21 @@
-const a = b
-const number = 0
+const express = require('express')
+const app = express()
+const port = 3000
+const productsRouter = require('./routers/productsRouter')
+
+app.use(express.static('public'));
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Entry point server')
+});
+
+app.use("/products", productsRouter)
+
+app.listen(port, () => {
+    console.log(`We are listening on post ${port}`);
+
+})
+
+
