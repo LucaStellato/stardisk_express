0c4551dc72623c2b71d3ad90ebde3d06926a6dc4
DarioConiglione <dariocng13@gmail.com>
2026-01-09 18:19:05 +0100

    create connection to db

---
+++ data/db.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Root',
+    database: 'stardisk_db'
+
+})
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to MySQL');
+
+})
+
+module.exports = connection;
