8bcd2336558fa40671f1367e17d81c84643c4109
gabrielemarchesano <marchesano.gabriele@gmail.com>
2026-01-11 17:48:16 +0100

    implement server setup with express, cors, and routing for products API

--- server.js
+++ server.js
@@ -1,2 +1,37 @@
-const a = b
-const number = 0
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+const cors = require("cors");
+
+// Routes
+const productsRouter = require("./routes/products");
+
+// Middlewares
+const notFound = require("./middlewares/notFound");
+const serverError = require("./middlewares/serverError");
+
+// CORS policy
+app.use(cors({
+  origin: "http://localhost:5173"
+}))
+
+// body parser
+app.use(express.json());
+
+// static assets folder
+app.use(express.static("public"));
+
+app.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Stardisk API Server");
+});
+
+app.use("/api/products", productsRouter);
+
+app.use(serverError);
+
+app.use(notFound);
