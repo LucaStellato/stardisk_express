2af51e5535b813198668cf0b6cd7b6a33826b9b3
gabrielemarchesano <marchesano.gabriele@gmail.com>
2026-01-11 18:06:57 +0100

    add product controller and crud index and show

---
+++ controllers/productController.js
@@ -0,0 +1,43 @@
+const connection = require("../db/connection");
+
+function index(req, res){
+  const sql = "SELECT * from products";
+
+  connection.query(sql, (err, results) => {
+    if(err){
+      return res.status(500).json({
+        error: true,
+        message: err.message
+      })
+    }
+    /* console.log(results); */
+    res.json(results);
+  })
+}
+  
+function show(req, res){
+  const id = Number(req.params.id);
+  const sql = "SELECT * from products WHERE id = ?";
+
+  connection.query(sql, [id], (err, results) => {
+    if(err){
+      return res.status(500).json({
+        error: true,
+        message: err.message
+      })
+    }
+    //console.log(results);
+    if(results.length === 0){
+      return res.status(404).json({
+        error: true,
+        message: "Product not found"
+      })
+    }
+    res.json(results);
+  })
+}
+
+module.exports = {
+  index,
+  show
+}
