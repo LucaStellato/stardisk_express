d067cd8cac8b92eac48b7b56a2f12d258d23c9b4
DarioConiglione <dariocng13@gmail.com>
2026-01-12 10:48:42 +0100

    add store product crud

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,10 +1,10 @@
 const connection = require("../database/connection");
-
-function index(req, res){
+//index
+function index(req, res) {
   const sql = "SELECT * from products";
 
   connection.query(sql, (err, results) => {
-    if(err){
+    if (err) {
       return res.status(500).json({
         error: true,
         message: err.message
@@ -14,20 +14,21 @@ function index(req, res){
     res.json(results);
   })
 }
-  
-function show(req, res){
+
+//show
+function show(req, res) {
   const id = Number(req.params.id);
   const sql = "SELECT * from products WHERE id = ?";
 
   connection.query(sql, [id], (err, results) => {
-    if(err){
+    if (err) {
       return res.status(500).json({
         error: true,
         message: err.message
       })
     }
     //console.log(results);
-    if(results.length === 0){
+    if (results.length === 0) {
       return res.status(404).json({
         error: true,
         message: "Product not found"
@@ -37,7 +38,40 @@ function show(req, res){
   })
 }
 
+//store
+function store(req, res) {
+
+  const sql = 'INSERT INTO products (name, slug, full_price, discount, amount, img_url, category, brand, color, rpm, release_year, description, id_artist, created_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+  const schema = [name, slug, full_price, discount, amount, img_url, category, brand, color, rpm, release_year, description, id_artist, created_at]
+  connection.query(sql, schema, (err, results) => {
+    if (err) {
+      console.error('Errore durante inserimento:', err);
+      return res.status(500).json({ error: 'Errore nel server' })
+    }
+
+    const newProduct = {
+      id: results.insertId,
+      name: req.body.name,
+      slug: req.body.slug,
+      full_price: req.body.full_price,
+      discount: req.body.discount,
+      amount: req.body.amount,
+      img_url: req.body.image,
+      category: req.body.category,
+      brand: req.body.brand,
+      color: req.body.color,
+      rpm: req.body.rpm,
+      release_year: req.bosy.release_year,
+      description: req.body.description,
+      id_artist: req.body.id_artist,
+      created_at: req.body.created_at
+    }
+    res.status(201).json(newProduct);
+  })
+}
+
 module.exports = {
   index,
-  show
+  show,
+  store
 }
