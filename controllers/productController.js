5cda234175749d0573203439f62d62b3c953ac62
matteovecchio04 <matteovex@gmail.com>
2026-01-13 12:57:02 +0100

    don't you EVER try to change products.name as products_name EVER AGAIN thx :)

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,10 +1,10 @@
 const connection = require("../database/connection");
 
-function index(req, res){
-  const sql = "SELECT products.id AS product_id, products.name AS product_name, products.full_price, products.discount, products.amount, products.img_url, products.category, products.brand, products.color, products.rpm, products.release_year, products.description, artists.name AS artist_name, genres.name as genre_name FROM products LEFT JOIN artists ON artists.id = products.id_artist LEFT JOIN product_genres ON product_genres.id_product = products.id LEFT JOIN genres ON genres.id = product_genres.id_genre";
+function index(req, res) {
+  const sql = "SELECT products.id AS product_id, products.name AS name, products.slug, products.full_price, products.discount, products.amount, products.img_url, products.category, products.brand, products.color, products.rpm, products.release_year, products.description, artists.name AS artist_name, genres.name as genre_name FROM products LEFT JOIN artists ON artists.id = products.id_artist LEFT JOIN product_genres ON product_genres.id_product = products.id LEFT JOIN genres ON genres.id = product_genres.id_genre";
 
   connection.query(sql, (err, results) => {
-    if(err){
+    if (err) {
       return res.status(500).json({
         error: true,
         message: err.message
@@ -14,20 +14,20 @@ function index(req, res){
     res.json(results);
   })
 }
-  
-function show(req, res){
+
+function show(req, res) {
   const id = Number(req.params.id);
-  const sql = "SELECT products.id AS product_id, products.name AS product_name, products.full_price, products.discount, products.amount, products.img_url, products.category, products.brand, products.color, products.rpm, products.release_year, products.description, artists.name AS artist_name, artists.description AS artist_description, artists.record_label, genres.name as genre_name FROM products LEFT JOIN artists ON artists.id = products.id_artist LEFT JOIN product_genres ON product_genres.id_product = products.id LEFT JOIN genres ON genres.id = product_genres.id_genre WHERE products.id = ?";
+  const sql = "SELECT products.id AS product_id, products.name AS name, products.full_price, products.discount, products.amount, products.img_url, products.category, products.brand, products.color, products.rpm, products.release_year, products.description, artists.name AS artist_name, artists.description AS artist_description, artists.record_label, genres.name as genre_name FROM products LEFT JOIN artists ON artists.id = products.id_artist LEFT JOIN product_genres ON product_genres.id_product = products.id LEFT JOIN genres ON genres.id = product_genres.id_genre WHERE products.id = ?";
 
   connection.query(sql, [id], (err, results) => {
-    if(err){
+    if (err) {
       return res.status(500).json({
         error: true,
         message: err.message
       })
     }
     //console.log(results);
-    if(results.length === 0){
+    if (results.length === 0) {
       return res.status(404).json({
         error: true,
         message: "Product not found"
