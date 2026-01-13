b0311a720a2e8969ad5b639b230d25115d3b82f0
gabrielemarchesano <marchesano.gabriele@gmail.com>
2026-01-13 11:06:17 +0100

    update index query to include artists and genres

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,7 +1,7 @@
 const connection = require("../database/connection");
 
 function index(req, res){
-  const sql = "SELECT * from products";
+  const sql = "SELECT products.id AS product_id, products.name AS product_name, products.full_price, products.discount, products.amount, products.img_url, products.category, products.brand, products.color, products.rpm, products.release_year, products.description, artists.name AS artist_name, genres.name as genre_name FROM products LEFT JOIN artists ON artists.id = products.id_artist LEFT JOIN product_genres ON product_genres.id_product = products.id LEFT JOIN genres ON genres.id = product_genres.id_genre";
 
   connection.query(sql, (err, results) => {
     if(err){
