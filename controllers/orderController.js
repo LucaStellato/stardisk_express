27d4cef20cc06d1df9a053c806058bc8f428df72
DarioConiglione <dariocng13@gmail.com>
2026-01-15 18:49:07 +0100

    upload store into pivot table product-orders

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,6 +1,6 @@
 const connection = require('../database/connection')
 
-function emailIsValid(mail){
+function emailIsValid(mail) {
     return /^[^\s@]+@[^\s@]+$/.test(mail)
 }
 
@@ -18,17 +18,34 @@ function show(req, res) {
 
 //store
 function store(req, res) {
-    const { mail, name, surname, address, total_price, free_shipment } = req.body;
+    const { mail, name, surname, address, total_price, free_shipment, products } = req.body;
 
+    //insert order
     const sql = 'INSERT INTO orders (mail, name, surname, address, total_price, free_shipment) VALUES (?, ?, ?, ?, ?, ?)'
-
     const array = [mail, name, surname, address, total_price, free_shipment]
 
     connection.query(sql, array, (err, results) => {
         if (err) return res.status(500).json({ error: err.message })
-        if(!emailIsValid(mail)) return res.status(422).json({ message: "Wrong email input"})
-        res.status(201).json({ message: 'Ordine creato!' })
+        if (!emailIsValid(mail)) return res.status(422).json({ message: "Wrong email input" })
+        /* res.status(201).json({ message: 'Ordine creato!' }) */
 
+        const orderID = results.insertId;
+        // insert into pivot
+        const sqlPivot = 'INSERT INTO product_orders (id_orders, id_product, products_amount) VALUES ?';
+
+        const pivotData = products.map(product => [
+            orderID,
+            product.id,
+            product.quantity
+        ])
+
+        connection.query(sqlPivot, [pivotData], (err) => {
+            if (err) {
+                return res.status(500).json({ error: err.message });
+            }
+
+        })
+        res.status(201).json({ success: true, order_id: orderID });
     })
 }
 
