fabdffc34f03ecfa4db9b1702650cc40060526fe
gabrielemarchesano <marchesano.gabriele@gmail.com>
2026-01-15 14:59:00 +0100

    add genre search query to product search

--- controllers/productController.js
+++ controllers/productController.js
@@ -136,11 +136,11 @@ function search(req, res){
     return res.json({message: "Enter more than 2 characters"})
   }
 
-  const sql = "SELECT DISTINCT products.name AS name, products.slug, products.img_url, products.full_price, artists.name AS artist_name FROM products LEFT JOIN artists ON products.id_artist = artists.id WHERE products.name LIKE ?";
-
+  const sql = "SELECT DISTINCT products.name AS name, products.slug, products.img_url, products.full_price, artists.name AS artist_name, genres.name AS genre_name FROM products LEFT JOIN artists ON products.id_artist = artists.id LEFT JOIN product_genres ON product_genres.id_product = products.id LEFT JOIN genres ON genres.id = product_genres.id_genre WHERE products.name LIKE ? OR genres.name LIKE ?";
+  
   const searchTerm = `%${query}%`;
 
-  connection.query(sql, [searchTerm], (err, results) => {
+  connection.query(sql, [searchTerm, searchTerm], (err, results) => {
     if(err) return res.status(500).json({
       error: true,
       message: err.message
